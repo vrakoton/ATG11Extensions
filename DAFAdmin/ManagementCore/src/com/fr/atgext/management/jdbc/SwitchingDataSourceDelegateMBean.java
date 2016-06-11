35e30124b5de3011194deaca34286281ca2f2d58
Vina <vrakoton@reenter.com>
2016-06-11 21:44:25 +0200

    adding mysql regexp_replace function to mimic Oracle's one!

--- DAFAdmin/ManagementCore/src/com/fr/atgext/management/jdbc/SwitchingDataSourceDelegateMBean.java
+++
@@ -1,8 +0,0 @@
-package com.fr.atgext.management.jdbc;
-
-public interface SwitchingDataSourceDelegateMBean {
-  public void prepareSwitch() throws Exception;
-  public void performSwitch();
-  public String getLiveDataStore();
-  public String getStagingDataStore();
-}
--- DAFAdmin/ManagementCore/src/com/fr/atgext/management/repository/RepositoryDelegateMBean.java
+++
@@ -1,8 +0,0 @@
-package com.fr.atgext.management.repository;
-
-public interface RepositoryDelegateMBean {
-  public String getRepositoryPath();
-  public String [] getItemDescriptorNames();
-  public String getStatisticsAsString();
-  public void invalidateCaches();
-}
